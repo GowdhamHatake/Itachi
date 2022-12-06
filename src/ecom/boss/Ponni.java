a3d21f8a3476beea500b045e2481a15f9d6241c3
GowdhamHatake <gowdhamandrewssingh@gmail.com>
2022-12-06 16:22:16 +0530

    First to mas

---
+++ src/ecom/boss/Ponni.java
@@ -0,0 +1,16 @@
+package ecom.boss;
+
+public class Ponni {
+	
+public static void main(String[] args) {
+	
+	System.out.println(1);
+	
+	System.out.println(2);
+	
+	System.out.println(3);
+	
+	
+}
+
+}
